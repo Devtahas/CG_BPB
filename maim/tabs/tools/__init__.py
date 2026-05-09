9f651915e01c33c64c817ccab6b4ba2f57249a11
RedCould <meysamsaiydavi2@gmail.com>
2026-05-10 03:19:19 +0330

    Create __init__.py

---
+++ maim/tabs/tools/__init__.py
@@ -0,0 +1,8 @@
+# tabs/tools/__init__.py
+from .generators import GeneratorsTab
+from .extractor import ExtractorTab
+from .port_scanner import PortScannerTab
+from .profile_maker import ProfileMakerTab
+from .datacenter_scanner import DatacenterScanner
+from .dns_scanner import DNSScanner
+from .fastly_scanner import FastlyScanner      # اضافه شد
